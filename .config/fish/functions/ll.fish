893c6fcb29869bb1b3598eaeb02081afd9ff83c9
raimu <raimu@tsubasa.xyz>
2020-11-28 09:33:27 +0100

    move fish functions into a single file

--- .config/fish/functions/ll.fish
+++
@@ -1,3 +0,0 @@
-function ll --description 'List contents of directory using long format'
-	exa -lgh $argv
-end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,15 @@
 function ls --description 'List contents of directory'
 	exa $argv
 end
+
+function ll --description 'List contents of directory using long format'
+	exa -lgh $argv
+end
+
+function la --description 'List contents of directory, including hidden files in directory using long format'
+	exa -lah $argv
+end
+
+function tree --description 'Lists directories recursively, and produces an indented listing of files'
+	exa -T $argv
+end
